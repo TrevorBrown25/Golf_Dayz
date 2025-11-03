e78f81ea573f56433ad3e5150bac2ce4ceb53dd2
TrevorBrown25 <trevorbrown25@gmail.com>
2025-11-03 13:26:40 -0600

    Configure router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import HomePage from './pages/HomePage';
 import RulesPage from './pages/RulesPage';
 
 function App() {
+  const basename = process.env.PUBLIC_URL
+    ? new URL(process.env.PUBLIC_URL, window.location.origin).pathname
+    : '/';
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/rules" element={<RulesPage />} />
